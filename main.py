87a6edc3a6c732ba59375ed475f4b42deb85054a
Morne Buitendag <morne.buitendag.personal@gmail.com>
2024-05-23 17:33:08 +0200

    Update main.py to v1.1.0

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 
-def check_val(val):
-	if type(val) != int:
-		return "Please enter a valid value"
-	elif val < 1 or val > 2:
-		return "Please enter a valid value"
-	else:
-		pass
+def add_new_data():
+	file = open("survey lung cancer.csv", "a")
+	file.write(f"{gender},{age},{smoking},{yellow_fingers},{anxiety},{peer_pressure},{chronic_disease},{fatigue},{allergy},{wheezing},{alcohol_consuming},{coughing},{shortness_of_breath},{swallowing_difficulty},{chest_pain},{prediction[0]}\n")
+	file.close()
 
 
 data = pd.read_csv("survey lung cancer.csv")
 
-features = ['AGE', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY', 'CHRONIC DISEASE', 'FATIGUE', 'ALLERGY', 'WHEEZING', 'ALCOHOL CONSUMING', 'COUGHING', 'SHORTNESS OF BREATH', 'CHEST PAIN']
+features = ['AGE', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY', 'PEER_PRESSURE', 'CHRONIC DISEASE', 'FATIGUE', 'ALLERGY', 'WHEEZING', 'ALCOHOL CONSUMING', 'COUGHING', 'SHORTNESS OF BREATH', 'SWALLOWING DIFFICULTY', 'CHEST PAIN']
 
 X = data[features]
 y = data["LUNG_CANCER"]
@@ -24,52 +21,63 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
-score = model.score(X_test, y_test)
+accuracy = model.score(X_test, y_test)
 
-print(f"Model Accuracy: {round(score * 100, 2)}%")
+print("\nPlease note that even though this test is anonymous, it still saves your answers in the data for better models.")
+print("\nAlso note that this test does not represent a medical diagnosis. If you think you have Lung Cancer, please contact your doctor.")
 
-print("Please enter the following details (1 for NO and 2 for YES)\n")
-age = int(input("How old are you: "))
+print(f"\nModel Accuracy: {round(accuracy * 100, 2)}%")
 
-if age < 18:
-	print("You are not eligible to test for Lung Cancer")
-	exit()
-elif age > 80:
-	print("You are not eligible to test for Lung Cancer")
-	exit()
-elif type(age) != int:
-	print("Please enter a valid age")
-	exit()
+gender = input("\nWhat is your gender (M for Male and F for Female): ").upper()
+
+while gender != "M" and gender != "F":
+
+	print("Only M and F are accepted")
+	gender = input("What is your gender: ").upper()
+
 else:
-	print("You are eligible to test for Lung Cancer")
 
-smoking = int(input("Are you a smoker: "))
-check_val(smoking)
-yellow_fingers = int(input("Do you have yellow fingers: "))
-check_val(yellow_fingers)
-anxiety = int(input("Do you have anxiety: "))
-check_val(anxiety)
-chronic_disease = int(input("Do you have a chronic disease: "))
-check_val(chronic_disease)
-fatigue = int(input("Do you experience fatigue: "))
-check_val(fatigue)
-allergy = int(input("Do you have an allergy: "))
-check_val(allergy)
-wheezing = int(input("Do you have wheezing: "))
-check_val(wheezing)
-alcohol_consuming = int(input("Are you consuming alcohol: "))
-check_val(alcohol_consuming)
-coughing = int(input("Do you have a cough: "))
-check_val(coughing)
-shortness_of_breath = int(input("Do you have shortness of breath: "))
-check_val(shortness_of_breath)
-chest_pain = int(input("Do you have chest pain: "))
-check_val(chest_pain)
+	print("\nPlease enter the following details (1 for NO and 2 for YES)\n")
 
-prediction = model.predict([[age, smoking, yellow_fingers, anxiety, chronic_disease, fatigue, allergy, wheezing, alcohol_consuming, coughing, shortness_of_breath, chest_pain]])
+	try:
 
-print("\nThis test is just for experimental use and does not represent a medical diagnosis. If you think you have Lung Cancer, please contact your doctor.\n")
-if prediction[0] == "YES":
-	print("Based on the information given, you might have Lung Cancer")
-else:
-	print("Based on the information given, it does not seem like you have Lung Cancer")
+		age = int(input("How old are you: "))
+
+		if age < 18 or age > 80:
+			print("You are not eligible for this test")
+			exit()
+		else:
+			print("You are eligible for this test\n")
+
+		smoking = int(input("Are you a smoker: "))
+		yellow_fingers = int(input("Do you have yellow fingers: "))
+		anxiety = int(input("Do you have anxiety: "))
+		peer_pressure = int(input("Have you experience peer pressure: "))
+		chronic_disease = int(input("Do you have a chronic disease: "))
+		fatigue = int(input("Do you experience fatigue: "))
+		allergy = int(input("Do you have an allergy: "))
+		wheezing = int(input("Do you have wheezing: "))
+		alcohol_consuming = int(input("Are you consuming alcohol: "))
+		coughing = int(input("Do you have a cough: "))
+		shortness_of_breath = int(input("Do you have shortness of breath: "))
+		swallowing_difficulty = int(input("Do you have difficulty swallowing: "))
+		chest_pain = int(input("Do you have chest pain: "))
+
+	except ValueError:
+
+		print("Please only enter numbers")
+		exit()
+
+	prediction = model.predict([[age, smoking, yellow_fingers, anxiety, peer_pressure, chronic_disease, fatigue, allergy, wheezing, alcohol_consuming, coughing, shortness_of_breath, swallowing_difficulty, chest_pain]])
+
+	if accuracy * 100 > 90:
+		add_new_data()
+	else:
+		pass
+
+	print("\nRemember that this test is just for experimental use and does not represent a medical diagnosis. If you think you have Lung Cancer, please contact your doctor.\n")
+
+	if prediction[0] == "YES":
+		print("Based on the information given, you might have Lung Cancer")
+	else:
+		print("Based on the information given, it does not seem like you have Lung Cancer")
